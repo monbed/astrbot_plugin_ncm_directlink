759dceb27db0fc7fa7e83b546798481931e0b42d
monbed <67903399+monbed@users.noreply.github.com>
2025-05-19 05:57:08 +0800

    Update main.py

--- main.py
+++ main.py
@@ -18,6 +18,7 @@ class DownloadMusicPlugin(Star):
         self._client = httpx.AsyncClient(
             headers={"User-Agent": "Mozilla/5.0 (Windows NT 6.1; Trident/7.0; rv:11.0) like Gecko"},
             timeout=10.0
+            verify=False
         )
 
     async def api_request(self, url: str, params: dict) -> dict:
